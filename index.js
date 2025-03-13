59ce30e63ca712e73c99a991e1ec4f28fc6f97a9
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-03-13 22:18:34 +0300

    jsfns

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun (venture ="roller-skate") {
+    return `This Saturday, I want to ${venture}!`;
+}
+
+function mondayWork(deed = "go to the office" ) {
+    return `This Monday, I will ${deed}.`;
+}
+
+function wrapAdjective(term= "*") {
+    return function (adjs= "special") {
+        return `You are ${term}${adjs}${term}!`
+    }
+}
+
+
